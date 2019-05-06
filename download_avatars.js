9bc3ad44274da6385554b3a259cdcc61a475f299
jitammy <jitammy@gmail.com>
2019-05-06 23:01:17 +0000

    step 7

--- download_avatars.js
+++ download_avatars.js
@@ -3,17 +3,30 @@ var secrets = require("./secrets")
 console.log('Welcome to the GitHub Avatar Downloader!');
 function getRepoContributors(repoOwner, repoName, cb) {
     var options = {
-        var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
         headers:{
             'User-Agent': 'request',
             'Authorization': secrets.GITHUB_TOKEN
         }
     }
-    request(url, function(err, res, body) {
-      cb(err, body);
+
+    request(options, function(err, res, body) {
+        if(err){
+            throw err
+        }
+        var data = JSON.parse(body)
+        var avatars = data.map((obj)=> obj.avatar_url)
+        cb(err, avatars);
     });
   }
+  
 getRepoContributors("jquery", "jquery", function(err, result) {
     console.log("Errors:", err);
     console.log("Result:", result);
 });
+
+function downloadImageByURL(url, filePath) {
+    // ...
+  }
+
+downloadImageByURL()
