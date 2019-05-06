a6486efe7dae2dc45fbb9be3eaeff6c83845b107
jitammy <jitammy@gmail.com>
2019-05-06 22:11:39 +0000

    adding headers

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,19 @@
 var request = require('request');
-
+var secrets = require("./secrets")
 console.log('Welcome to the GitHub Avatar Downloader!');
+function getRepoContributors(repoOwner, repoName, cb) {
+    var options = {
+        var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+        headers:{
+            'User-Agent': 'request',
+            'Authorization': secrets.GITHUB_TOKEN
+        }
+    }
+    request(url, function(err, res, body) {
+      cb(err, body);
+    });
+  }
+getRepoContributors("jquery", "jquery", function(err, result) {
+    console.log("Errors:", err);
+    console.log("Result:", result);
+});
