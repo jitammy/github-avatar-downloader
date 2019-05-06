f9073c45496abb4775be9d73df84c85947cee7ec
jitammy <jitammy@gmail.com>
2019-05-06 23:31:29 +0000

    step 7

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,9 @@
 var request = require('request');
 var secrets = require("./secrets")
-console.log('Welcome to the GitHub Avatar Downloader!');
+var fs = require('fs')
+
+
+// console.log('Welcome to the GitHub Avatar Downloader!');
 function getRepoContributors(repoOwner, repoName, cb) {
     var options = {
         url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
@@ -9,7 +12,6 @@ function getRepoContributors(repoOwner, repoName, cb) {
             'Authorization': secrets.GITHUB_TOKEN
         }
     }
-
     request(options, function(err, res, body) {
         if(err){
             throw err
@@ -19,14 +21,20 @@ function getRepoContributors(repoOwner, repoName, cb) {
         cb(err, avatars);
     });
   }
-  
 getRepoContributors("jquery", "jquery", function(err, result) {
     console.log("Errors:", err);
     console.log("Result:", result);
 });
 
 function downloadImageByURL(url, filePath) {
-    // ...
+   
+    request.get(url)               
+    .on('error', function (err) {                                   
+      throw err; 
+    })
+    .on('response', function (response) {                           
+      console.log('Response Status Code: ', response.statusCode);
+    })
+    .pipe(fs.createWriteStream('./avatars'+'/'+filePath));     
   }
-
-downloadImageByURL()
+downloadImageByURL('https://avatars3.githubusercontent.com/u/192451?v=4', 1)
